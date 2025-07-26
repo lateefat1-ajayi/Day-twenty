b8858c514037401cdc5658360a35174d744fd106
lateefat <ajayilateefat09@gmail.com>
2025-07-26 16:32:26 -0700

    adding frontend url in backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,23 @@ const { errorHandler } = require("./middleware/errorHandler");
 const app = express();
 connectDB();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  'https://day-twenty-1.onrender.com', // frontend URL
+  'http://localhost:5173' 
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+); 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
